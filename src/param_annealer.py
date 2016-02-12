44d1b978605c4b672ccde993298afc11f962db63
mat kelcey <matthew.kelcey@gmail.com>
2016-02-11 21:01:39 -0800

    allow annealing of params down as well as up

--- src/param_annealer.py
+++ src/param_annealer.py
@@ -13,14 +13,13 @@ parser.add_argument('--param', type=str, help="rospy param to set")
 parser.add_argument('--burn-in', type=float, default=0, help="burn in (sec) to keep at min-value")
 parser.add_argument('--ramp-up', type=float, help="ramp-up time (sec) after burn-in to move from min to max")
 parser.add_argument('--step', type=float, help="granularity of steps (sec). i.e. emit each step sec")
-parser.add_argument('--min-value', type=float, help="min value to emit <= burn-in sec")
-parser.add_argument('--max-value', type=float, help="max value to emit >= burn-in + ramp-up sec")
+parser.add_argument('--start-value', type=float, help="start value to emit <= burn-in sec")
+parser.add_argument('--end-value', type=float, help="final value to emit >= burn-in + ramp-up sec")
 o = parser.parse_args()
 print "OPTS", o
 assert o.burn_in >= 0
 assert o.ramp_up > 0
 assert o.step > 0
-assert o.min_value < o.max_value
 
 def emit(n, v):
     print datetime.datetime.now().strftime('%c'), v
@@ -29,13 +28,13 @@ def emit(n, v):
 n = 0.0
 while n < o.burn_in + o.ramp_up + o.step:
     if n <= o.burn_in:
-        emit(n, o.min_value)
+        emit(n, o.start_value)
     elif n >= o.burn_in + o.ramp_up:
-        emit(n, o.max_value)
+        emit(n, o.end_value)
     else:
         normalised = float(n - o.burn_in) / o.ramp_up
-        rescaled = (normalised * (o.max_value - o.min_value)) + o.min_value
+        rescaled = (normalised * (o.end_value - o.start_value)) + o.start_value
         emit(n, rescaled)
-    n += o.step
+    n += 1
     # assume everything else is instant (including set_param call)
     time.sleep(o.step)
